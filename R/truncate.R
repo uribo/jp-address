aa743100224d28ab1b401adcf35330e30b1822a0
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-04-11 16:36:27 +0900

    Oops

--- R/truncate.R
+++ R/truncate.R
@@ -9,12 +9,12 @@ truncate_address <- function(x, level = "city") {
       purrr::pluck(1)
   } else if (rlang::is_true(is_include_level_char(x))) {
     
-    if (stringr::str_detect(x, ""))
+    if (stringr::str_detect(x, ".+郡村田町"))
       stringr::str_split(x,
                          pattern = "(?<=[[:print:]]郡村田町)") %>% 
       purrr::pluck(1) %>% 
       purrr::pluck(1)
-    else 
+    else
       stringr::str_split(x,
                          pattern = "(?<=[[:print:]]市市|[[:print:]]市町|[[:print:]]町町|[[:print:]]郡村)", n = 2) %>% 
       purrr::pluck(1) %>% 
