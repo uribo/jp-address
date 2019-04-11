d959e407a7e40c4cb422c58e829f612e51382a83
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-04-11 16:44:19 +0900

    山形県西村山郡などに対応

--- R/truncate.R
+++ R/truncate.R
@@ -9,9 +9,9 @@ truncate_address <- function(x, level = "city") {
       purrr::pluck(1)
   } else if (rlang::is_true(is_include_level_char(x))) {
     
-    if (stringr::str_detect(x, ".+郡村田町"))
+    if (stringr::str_detect(x, ".+(郡村田町|村山郡)"))
       stringr::str_split(x,
-                         pattern = "(?<=[[:print:]]郡村田町)") %>% 
+                         pattern = "(?<=[[:print:]]郡村田町|[[:print:]]村山郡)") %>% 
       purrr::pluck(1) %>% 
       purrr::pluck(1)
     else
