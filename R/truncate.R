a846f9483f4de7fafb98d065dbae80a6eb7cb1c5
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-04-11 18:01:27 +0900

    市区町村のいずれかの文字を持つ特殊パターンに対応

--- R/truncate.R
+++ R/truncate.R
@@ -9,14 +9,14 @@ truncate_address <- function(x, level = "city") {
       purrr::pluck(1)
   } else if (rlang::is_true(is_include_level_char(x))) {
     
-    if (stringr::str_detect(x, ".+(郡村田町|村山郡)"))
+    if (stringr::str_detect(x, "佐波郡玉村町|大村市|.+(郡市貝町|郡赤井川村|郡村田町|村山郡|市郡明日香村)"))
       stringr::str_split(x,
-                         pattern = "(?<=[[:print:]]郡村田町|[[:print:]]村山郡)") %>% 
+                         pattern = "(?<=佐波郡玉村町|大村市|[[:print:]]郡市貝町|[[:print:]]郡村田町|[[:print:]]村山郡(河北町|西川町|朝日町|大江町)|高市郡明日香村|余市郡赤井川村)") %>% 
       purrr::pluck(1) %>% 
       purrr::pluck(1)
     else
       stringr::str_split(x,
-                         pattern = "(?<=[[:print:]]市市|[[:print:]]市町|[[:print:]]町町|[[:print:]]郡村)", n = 2) %>% 
+                         pattern = "(?<=薩摩川内市|安来市|木更津市|[[:print:]]市市|[[:print:]]市町|[[:print:]]町市|[[:print:]]町町|[[:print:]]郡村)", n = 2) %>% 
       purrr::pluck(1) %>% 
       purrr::pluck(1)
   } else {
