567af89c846126639ffaa82bbc72dcffee4584da
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-04-12 14:12:19 +0900

    都道府県名から始まる住所を県と以下のレベルに分割する

--- R/modified_address.R
+++ R/modified_address.R
@@ -7,3 +7,10 @@ modified_address  <- function(city, adds, target_city, fix_adds, replace = NULL)
     stringr::str_replace(adds, glue::glue(fix_adds, ".+"), replace),
     adds)  
 }
+
+separate_address_cols <- function(df, col, into) {
+  df %>% 
+    tidyr::separate(col = col,
+                    into = into,
+                    sep = "(?<=東京都|北海道|(大阪|京都)府|(青森|岩手|宮城|秋田|山形|福島|茨城|栃木|群馬|埼玉|千葉|神奈川|新潟|富山|石川|福井|山梨|長野|岐阜|静岡|愛知|三重|滋賀|兵庫|奈良|和歌山|鳥取|島根|岡山|広島|山口|徳島|香川|愛媛|高知|福岡|佐賀|長崎|熊本|大分|宮崎|鹿児島|沖縄)県)")
+}
