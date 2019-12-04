8c143e137fe1b3b7aae14cdb89ba14791aed5973
Shinya Uryu <uryu.shinya@nies.go.jp>
2019-12-04 14:40:00 +0900

    「読み仮名データの促音・拗音を小書きで表記しないもの」と「ローマ字」対応

--- R/read_zipcode.R
+++ R/read_zipcode.R
@@ -29,24 +29,49 @@ read_zipcode_jigyosyo <- function(path) {
     stringi::stri_trans_general,
     id = "nfkc")
 }
-# type... oogaki, kogaki, jigyosyo
 # ref) https://www.post.japanpost.jp/zipcode/dl/readme.html
-read_zipcode <- function(path, type = c("kogaki")) {
+read_zipcode <- function(path, type = c("oogaki", "kogaki", "roman")) {
+  rlang::arg_match(type)
   address_level <- c("prefecture", "city", "street")
-  dplyr::mutate_if(readr::read_csv(path, 
-                                     locale = readr::locale(encoding = "cp932"),
-                                     col_names = c("jis_code", "old_zip_code", "zip_code", 
-                                                   paste0(address_level, "_kana"),
-                                                   address_level,
-                                                   # is_ はそれぞれ 1... 該当、0... 該当せず
-                                                   "is_street_duplicate", # 一町域が二以上の郵便番号で表される場合の表示
-                                                   "is_banchi", # 小字毎に番地が起番されている町域の表示
-                                                   "is_cyoumoku", # 丁目を有する町域の場合の表示 
-                                                   "is_zipcode_duplicate", # 一つの郵便番号で二以上の町域を表す場合の表示
-                                                   "status", # 更新の表示 0... 更新なし, 1... 変更あり, 2... 廃止
-                                                   "modify_type" # 変更理由 0... 変更なし, 1... 市政・区政・町政・分区・政令指定都市施行, 2... 住居表示の実施, 3... 区画整理, 4... 郵便区調整等, 5... 訂正, 6... 廃止
-                                                   ),
-                                     col_types = "ccccccccclllddd"),
+  col_vars <- 
+    list(yomi = c("jis_code", "old_zip_code", "zip_code", 
+                  paste0(address_level, "_kana"),
+                  address_level,
+                  # is_ はそれぞれ 1... 該当、0... 該当せず
+                  "is_street_duplicate", # 一町域が二以上の郵便番号で表される場合の表示
+                  "is_banchi", # 小字毎に番地が起番されている町域の表示
+                  "is_cyoumoku", # 丁目を有する町域の場合の表示 
+                  "is_zipcode_duplicate", # 一つの郵便番号で二以上の町域を表す場合の表示
+                  "status", # 更新の表示 0... 更新なし, 1... 変更あり, 2... 廃止
+                  "modify_type" # 変更理由 0... 変更なし, 1... 市政・区政・町政・分区・政令指定都市施行, 2... 住居表示の実施, 3... 区画整理, 4... 郵便区調整等, 5... 訂正, 6... 廃止
+    ),
+    roman = c("zip_code",
+              address_level,
+              paste0(address_level, "_roman"))
+    )
+  
+  if (type == "oogaki") {
+    df <- 
+      readr::read_csv(path,
+                    locale = readr::locale(encoding = "cp932"),
+                    col_names = col_vars$yomi,
+                    col_types = "ccccccccclllddd")
+  } else if (type == "kogaki") {
+    df <- 
+      readr::read_csv(path, 
+                      locale = readr::locale(encoding = "cp932"),
+                      col_names = col_vars$yomi,
+                      col_types = "ccccccccclllddd")
+  } else if (type == "roman") {
+    df <- 
+      dplyr::mutate_at(readr::read_csv(path,
+                                       locale = readr::locale(encoding = "cp932"),
+                                       col_names = col_vars$roman,
+                                       col_types = "ccccccc"),
+                       dplyr::vars(tidyselect::ends_with("roman")),
+                       stringr::str_to_title)
+  }
+  dplyr::mutate_if(df,
                      is.character,
                      stringi::stri_trans_general,
                      id = "nfkc")
--- data-raw/japan-address.R
+++ data-raw/japan-address.R
@@ -22,18 +22,41 @@ if (length(fs::dir_ls(here::here("data-raw"), regexp = "japanpost_")) != 3) {
     stringr::str_subset("bangobo", negate = TRUE) %>% 
     paste0(japan_post_site, .) %>% 
     ensure(length(.) == 4L)
-  # 1. 住所の郵便番号 --------------------------------------------------------------
+  # 1. 住所の郵便番号 (oogaki, kogaki) --------------------------------------------------------------
   if (dir.exists(here::here("data-raw/japanpost_kogaki")) == FALSE)
     dir.create(here::here("data-raw/japanpost_kogaki"))
-  if (length(fs::dir_ls(here::here("data-raw/japanpost_kogaki/"), regexp = ".CSV$")) != 47) {
+  if (dir.exists(here::here("data-raw/japanpost_oogaki")) == FALSE)
+    dir.create(here::here("data-raw/japanpost_oogaki"))
+  if (length(fs::dir_ls(here::here("data-raw/japanpost_oogaki/"), regexp = ".CSV$")) != 48) {
+    target_files <- 
+      target_urls %>% 
+      stringr::str_subset("oogaki-zip.html") %>% 
+      read_html() %>% 
+      html_nodes(css = '#main-box > div > table:nth-child(7) > tbody > tr > td > a') %>% 
+      html_attr(name = "href") %>% 
+      ensure(length(.) == 48L)
+    target_files %>% 
+      xml2::url_absolute(base = glue::glue(japan_post_site, "/zipcode/dl/")) %>% 
+      purrr::walk(
+        ~ curl::curl_download(.x,
+                              destfile = here::here("data-raw/japanpost_oogaki",
+                                                    basename(.x))))
+    here::here("data-raw/japanpost_oogaki/", basename(target_files)) %>% 
+      purrr::walk(
+        ~ unzip(zipfile = .x, 
+                exdir = here::here("data-raw/japanpost_oogaki"),
+                overwrite = TRUE))
+    unlink(fs::dir_ls(here::here("data-raw/japanpost_oogaki"), 
+                      regexp = ".zip$"))
+  }
+  if (length(fs::dir_ls(here::here("data-raw/japanpost_kogaki/"), regexp = ".CSV$")) != 48) {
     target_files <- 
       target_urls %>% 
       stringr::str_subset("kogaki-zip.html") %>% 
       read_html() %>% 
       html_nodes(css = '#main-box > div > table:nth-child(7) > tbody > tr > td > a') %>% 
       html_attr(name = "href") %>% 
-      stringr::str_subset("ken_all.zip", negate = TRUE) %>% 
-      ensure(length(.) == 47L)
+      ensure(length(.) == 48L)
     target_files %>% 
       xml2::url_absolute(base = glue::glue(japan_post_site, "/zipcode/dl/")) %>% 
       purrr::walk(
@@ -74,6 +97,29 @@ if (length(fs::dir_ls(here::here("data-raw"), regexp = "japanpost_")) != 3) {
   }
   unlink(here::here("data-raw/japanpost_jigyosyo/", 
                     basename(target_files)))
+  # 3. ローマ字 -----------------------------------------------------------------
+  if (dir.exists(here::here("data-raw/japanpost_roman")) == FALSE)
+    dir.create(here::here("data-raw/japanpost_roman"))
+  if (length(fs::dir_ls(here::here("data-raw/japanpost_roman"), regexp = ".CSV$")) != 1L) {
+    target_files <- 
+      target_urls %>% 
+      stringr::str_subset("roman-zip.html") %>% 
+      read_html() %>% 
+      html_nodes(css = '#main-box > div > p:nth-child(9) > a') %>% 
+      html_attr(name = "href") %>% 
+      ensure(length(.) == 1L)
+    target_files %>% 
+      glue::glue(glue::glue(japan_post_site, "/zipcode/dl/"), .) %>% 
+      curl::curl_download(url = .,
+                          destfile = paste0(here::here("data-raw/japanpost_roman/"), 
+                                            gsub("\\?.+", "", basename(.))))
+    unzip(zipfile = here::here("data-raw/japanpost_roman/", 
+                               gsub("\\?.+", "", basename(target_files))), 
+          exdir = here::here("data-raw/japanpost_roman"),
+          overwrite = TRUE)
+    unlink(here::here("data-raw/japanpost_roman/", 
+                      gsub("\\?.+", "", basename(target_files))))
+  }
 }
 
 df_japanpost_zip <-
